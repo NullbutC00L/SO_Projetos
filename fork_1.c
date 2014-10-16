b40a07178633069e1e9d651e27dfe9ccd7494086
tonifreire <tonifreire93@gmail.com>
2014-10-16 23:45:29 +0100

    fork final e bem feito

--- fork_1.c
+++ fork_1.c
@@ -3,48 +3,39 @@
 # include <stdio.h>
 # include <time.h>
 
-
-
-int Pid,PidTerminou, status;
-time_t init,fim;
 int main()
-{	
+{
+	int Pid,PidTerminou, status;
+	time_t init,fim;	
 
 
 	init= time(NULL);
-	printf("tempo inicial-%ld\n",init);
-	for (int i = 0; i < 10; ++i)
+	for (int i = 0; i < 10; i++)
 	{
 
-			if((Pid=fork())==0){
+		if((Pid=fork())==0){
 
-				printf("sou o processo filho\n");
-				execl("escritor","escritor",0);
-				printf("\n");
-				printf("\n");
+			printf("O processo filho vai-se executar o escritor... \n");
+			execl("escritor","escritor",0);
+				
 
-				exit(-1);
+			exit(-1);
 			}
 
-			else{
-
-				
-				printf("\n");
-
-				printf("criou um subprocesso de indentificador = %d\n",Pid);
-				PidTerminou= wait(&status);
-
-				printf("terminou um subprocesso de indentificador = %d\n",PidTerminou);
-				
-
-				
-			}
+		else if(Pid==-1)
+			return -1;
+	}
+	
+	
+	for (int i = 0; i < 10; i++)
+	{
+		PidTerminou= wait(&status);
 	}
 
 	fim= time(NULL);
-	printf("tempo final-%ld\n",fim);
-	printf("tempo total-%ld\n",fim-init );
-	return 0;
-	
+	printf("\nO tempo total de execucao: %ld\n",fim-init );
 
+
+
+	return 1;
 }
